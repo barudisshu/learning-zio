2e612df0ce7fb2eeb4f595c1bc269d738433be05
ehcayen <yanyi.x.chen@ericsson.com>
2021-03-23 15:44:26 +0800

    netty channel

--- chap04/src/main/scala/info/galudisu/netty/Main.scala
+++ chap04/src/main/scala/info/galudisu/netty/Main.scala
@@ -1,10 +1,44 @@
 package info.galudisu.netty
+import info.galudisu.netty.Channel.{Channel, eventLoop, localAddress}
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.channel.{ReflectiveChannelFactory, Channel => JChannel}
 import zio._
+import zio.blocking.Blocking
+import zio.clock.Clock
 import zio.console._
 
+import java.net.InetSocketAddress
+
 object Main extends App {
+
+  val workerGroup                  = new NioEventLoopGroup()
+  val channel: JChannel            = new ReflectiveChannelFactory[JChannel](classOf[NioServerSocketChannel]).newChannel()
+  val liveChannel: ULayer[Channel] = Channel.live(channel)
+
+  val env = Console.live ++ Clock.live ++ Blocking.live ++ liveChannel ++ (liveChannel >>> Channel.Unsafe.live)
+
+  val program: ZIO[Console with Clock with Blocking with Channel with Channel.Unsafe, Throwable, Unit] = {
+
+    def loop(): ZIO[Console with Blocking with Channel with Channel.Unsafe, Throwable, Unit] = {
+      {
+        for {
+          jChannel <- Channel.read()
+          _        <- Channel.close()
+        } yield jChannel
+      } *> loop()
+    }
+
+    for {
+      _              <- Channel.Unsafe.beginRead()
+      channelPromise <- Channel.Unsafe.voidPromise()
+      _              <- Channel.Unsafe.register(workerGroup.next(), channelPromise)
+      _              <- Channel.Unsafe.bind(new InetSocketAddress(8080), channelPromise)
+      _              <- loop()
+      _              <- eventLoop.map(_.shutdownGracefully())
+    } yield ()
+  }
+
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
-    (for {
-      _ <- putStrLn("hello world")
-    } yield ()).exitCode
+    program.provideLayer(env).exitCode
 }
